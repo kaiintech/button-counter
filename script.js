441679c1e4e24417365b4a7134ea60731dc39c4e
kaiintech <kaiintech09@gmail.com>
2025-09-19 20:19:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const counterDisplay = document.querySelector(".update-counter");
-const counterBtn = document.querySelector(".counter");
+const decreaseBtn = document.querySelector(".decrease");
+const increaseBtn = document.querySelector(".increase");
 const resetBtn = document.querySelector(".reset");
 
 let count = 0;
 
-counterBtn.addEventListener("click", () => {
-    count++;
-    counterDisplay.textContent = count;
+increaseBtn.addEventListener("click", () => {
+  count++;
+  counterDisplay.textContent = count;
+});
+
+decreaseBtn.addEventListener("click", () => {
+  count--;
+  counterDisplay.textContent = count;
 });
 
 resetBtn.addEventListener("click", () => {
-    count = 0;
-    counterDisplay.textContent = count;
-})
+  count = 0;
+  counterDisplay.textContent = count;
+});
